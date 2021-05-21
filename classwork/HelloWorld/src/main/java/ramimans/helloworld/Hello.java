68a0352b0cc704e8eef803d2fa9d37cd4310c6a5
Rami Mansieh <rmansieh@gmail.com>
2021-05-20 23:42:30 -0400

    say Hello World in Netbeans

---
+++ classwork/HelloWorld/src/main/java/ramimans/helloworld/Hello.java
@@ -0,0 +1,17 @@
+package ramimans.helloworld;
+
+/**
+ *
+ * @author Rami Mansieh
+ * email: rmansieh@gmail.com
+ * data: May 20, 2021
+ * purpose: Say hello to the world and myself
+ */
+
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Rami!");
+    }
+}
+
